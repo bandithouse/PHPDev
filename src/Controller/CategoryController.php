b5df6c5c3d779902ac4d9be29ddcf155bbf53de3
Marek Rynarzewski <marekrynarzewski@gmail.com>
2022-01-20 15:26:44 +0100

    add category

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryForm;
+use App\Service\CategoryManager;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * @Route("/api/category", name="app_category_")
+ */
+class CategoryController extends AppController
+{
+
+    /**
+     * @Route("", name="add", methods={"POST"})
+     * @param Request $request
+     * @param CategoryManager $categoryManager
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
+    public function addNewAction(
+        Request $request,
+        CategoryManager $categoryManager,
+        SerializerInterface $serializer
+    ): JsonResponse
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryForm::class, $category);
+        $content = json_decode($request->getContent(), true);
+        $form->submit($content);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $categoryManager->saveCategory($category);
+
+            return new JsonResponse($serializer->serialize($category, 'json'), 200, [], true);
+        }
+
+        return new JsonResponse(['errors' => $this->handleErrors($form)], 400);
+    }
+}
